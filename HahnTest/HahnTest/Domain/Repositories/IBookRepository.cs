6d648aea384f030b9bd5a22f98592d0bee4fc188
Victor Hugo Montano <victorhm2011@gmail.com>
2023-05-07 22:16:24 -0400

    [Task-01]: Addition of Book interface

---
+++ HahnTest/HahnTest/Domain/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using HahnTest.Domain.Entities;
+
+namespace HahnTest.Domain.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<List<Book>> GetAllAsync();
+        Task<Book> GetByIdAsync(int id);
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+        Task DeleteAsync(Book book);
+    }
+}
