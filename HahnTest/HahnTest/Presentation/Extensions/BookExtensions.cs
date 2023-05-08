cb91f6aed63c3fc45ab050fbdac0bf85a5dcf295
Victor Hugo Montano <victorhm2011@gmail.com>
2023-05-08 00:27:13 -0400

    [Task-01]: Addition of Book extensions

---
+++ HahnTest/HahnTest/Presentation/Extensions/BookExtensions.cs
@@ -0,0 +1,18 @@
+﻿using HahnTest.Domain.Entities;
+using HahnTest.Presentation.DTOs;
+
+namespace HahnTest.Presentation.Extensions
+{
+    public static class BookExtensions
+    {
+        public static List<BookDto> ToDto(this List<Book> books)
+        {
+            return books.Select(x => x.ToDto()).ToList();
+        }
+
+        public static BookDto ToDto(this Book book)
+        {
+            return new BookDto(book);
+        }
+    }
+}
