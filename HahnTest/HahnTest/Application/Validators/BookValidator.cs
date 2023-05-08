75d201e9ecec4dc272f49a75ebbeb2c357b6430b
Victor Hugo Montano <victorhm2011@gmail.com>
2023-05-08 00:27:31 -0400

    [Task-01]: Addition of Book validator

---
+++ HahnTest/HahnTest/Application/Validators/BookValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using HahnTest.Presentation.DTOs;
+
+namespace HahnTest.Application.Validators
+{
+    public class BookValidator : AbstractValidator<BookDto>
+    {
+        public BookValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Author).NotEmpty();
+            RuleFor(x => x.Price).GreaterThan(0);
+            RuleFor(x => x.PublishDate).NotEmpty();
+            RuleFor(x => x.Isbn).NotEmpty().Length(13);
+        }
+    }
+}
