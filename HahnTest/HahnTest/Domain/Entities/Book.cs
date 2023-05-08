617bb019ba158ac126624962ea2928ac72e78082
Victor Hugo Montano <victorhm2011@gmail.com>
2023-05-07 22:08:18 -0400

    [Task-01]: Creation of book class

---
+++ HahnTest/HahnTest/Domain/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace HahnTest.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
