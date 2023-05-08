f7574ab5ed39c9aac858e9e188b2d2562b27e1cf
Victor Hugo Montano <victorhm2011@gmail.com>
2023-05-07 23:30:00 -0400

    [Task-01]: Addition of Book Service interface

---
+++ HahnTest/HahnTest/Domain/Services/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using HahnTest.Domain.Entities;
+
+namespace HahnTest.Domain.Services
+{
+    public interface IBookService
+    {
+        Task<List<Book>> GetAllAsync();
+        Task<Book> GetByIdAsync(int id);
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+        Task DeleteAsync(Book book);
+    }
+}
