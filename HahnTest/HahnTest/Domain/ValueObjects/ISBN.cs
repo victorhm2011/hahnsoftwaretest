9e8f01d0f84475d9a9795716e4c5de0b82cd6081
Victor Hugo Montano <victorhm2011@gmail.com>
2023-05-07 22:12:56 -0400

    [Task-01]: Addition of ISBN class

---
+++ HahnTest/HahnTest/Domain/ValueObjects/ISBN.cs
@@ -0,0 +1,19 @@
+﻿namespace HahnTest.Domain.ValueObjects
+{
+    public class ISBN
+    {
+        private string _value;
+
+        public ISBN(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("ISBN cannot be empty or null.");
+            }
+
+            _value = value;
+        }
+
+        public override string ToString() => _value;
+    }
+}
