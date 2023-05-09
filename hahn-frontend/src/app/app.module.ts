d33d6f9c54972e629f6be709d31517fd0d46c347
Victor Hugo Montano <victorhm2011@gmail.com>
2023-05-09 15:33:48 -0400

    [Task-02]: Create book form component

--- hahn-frontend/src/app/app.module.ts
+++ hahn-frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BookListComponent } from './components/book-list/book-list.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    BookFormComponent
   ],
   imports: [
     BrowserModule,
