bbb5db5c27be82b936cf0e05c411fa4eab6756f9
Victor Hugo Montano <victorhm2011@gmail.com>
2023-05-09 23:18:45 -0400

    [Task-02]: Add book constants

---
+++ hahn-frontend/src/assets/constants/book.constants.ts
@@ -0,0 +1,6 @@
+export class BookConstants {
+    public static TITLE: string = "Title";
+    public static AUTHOR: string = "Author";
+    public static PRICE: string = "Price";
+    public static PUBLISHDATE: string = "Publish Date";
+}
