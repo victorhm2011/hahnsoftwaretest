e4f34109489ff931d017cb1562197abceb49b949
Victor Hugo Montano <victorhm2011@gmail.com>
2023-05-09 22:54:57 -0400

    [Task-02]: Add book service methods

--- hahn-frontend/src/app/services/book.service.ts
+++ hahn-frontend/src/app/services/book.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  private apiUrl = `${environment.apiUrl}/books`;
+
+  constructor(private http: HttpClient) { }
+
+  getAllBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  getBookById(id: string): Observable<Book> {
+    return this.http.get<Book>(`${this.apiUrl}/${id}`);
+  }
+
+  createBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+
+  updateBook(book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.apiUrl}/${book.id}`, book);
+  }
+
+  deleteBook(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 }
